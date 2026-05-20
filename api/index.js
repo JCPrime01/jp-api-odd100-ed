92f7c49e63aee25a7eea03c50db1adf8d7643c29
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 21:50:48 -0300

    chore: update shareCode to FKBP4L4NFSS

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=RK66FX0EHLN&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=FKBP4L4NFSS&afp=tapi100' });
     res.end();
   }
