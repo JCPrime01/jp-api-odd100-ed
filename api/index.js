2a56e2dc7e317f2d33d4c77b10a428275f268b53
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 15:50:52 -0300

    chore: update shareCode to NZN1HZOPNY5

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=Y1WT1QCRWAD&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=NZN1HZOPNY5&afp=tapi100' });
     res.end();
   }
